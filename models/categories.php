1d86f8d352da370dd092daec16229da3c8390db8
Blaser993 <corradodepinto.design@gmail.com>
2023-03-31 17:11:01 +0200

    PUSH PRE TICKET

--- models/categories.php
+++ models/categories.php
@@ -1,12 +1,17 @@
 <?php
 
 class Categories{
-    public $animale;
-    public $icon; 
+    public $animale = ["Cani", "Gatti"];
+    public $icon = ["icon1","icon2"]; 
 
-    public function __construct($_animale, $_icon){
+    public function __construct(){
+    }
 
+    function setCategoria($_animale, $_icon) {
         $this->animale = $_animale;
         $this->icon = $_icon;
+        return $_icon;
     }
+
+
 }
--- models/products.php
+++ models/products.php
@@ -1,15 +1,19 @@
 <?php
 
+require_once __DIR__."./Categories.php";
+
 class Products {
     public $nome;
     public $prezzo;
     public $tipologia;
+    public Categories $categoria;
     public $immagine;
 
 
-    public function __construct($_nome, $_prezzo, $_tipologia){
+    public function __construct($_nome, $_prezzo, $_tipologia, Categories $_categoria){
         $this->nome = $_nome;
         $this->prezzo = $_prezzo."€";
         $this->tipologia = $_tipologia;
+        $this->categoria = $_categoria;
     }
 }
